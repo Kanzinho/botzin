c0e82866b4f092eb90dd8e51c2c7a5c009a6d00b
Kanzinho <80663033+Kanzinho@users.noreply.github.com>
2021-04-15 07:05:12 -0300

    Create geral2.js

---
+++ src/geral2.js
@@ -0,0 +1,24 @@
+const geral = (prefix) => {
+
+ return `
+╔─━━━[ GERAL  ]━━━─╗
+║
+║ *[1]*${prefix}Figu ou ${prefix}sticker
+║ *[2]*${prefix}Brainly
+║ *[3]*${prefix}Toimg
+║ *[4]*${prefix}Ytsearch
+║ *[5]*${prefix}Darkjokes
+║ *[6]*${prefix}Ytmp4
+║ *[7]*${prefix}Tts
+║ *[8]*${prefix}Tomp3
+║ *[9]*${prefix}Url2img
+║ *[10]*${prefix}Dono
+║ *[11]*${prefix}Dono2
+║ *[12]*${prefix}Ping
+║ *[13]*${prefix}Gay [@]
+║ *[14]*${prefix}Wame ou ${prefix}wa.me
+║
+╚─━━[ KAZINHO BOT ]━━─╝`
+}
+
+exports.geral = geral
