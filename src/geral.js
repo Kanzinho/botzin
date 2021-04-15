758e0ddc76b3af8104b23e8204224b66b593b71d
Kanzinho <80663033+Kanzinho@users.noreply.github.com>
2021-04-15 08:05:53 -0300

    Update geral.js

--- src/geral.js
+++ src/geral.js
@@ -4,19 +4,33 @@ const geral = (prefix) => {
 ╔─━━━[ GERAL  ]━━━─╗
 ║
 ║ *[1]*${prefix}Figu ou ${prefix}sticker
+║ *Nota:* faz uma figurinha
 ║ *[2]*${prefix}Brainly
+║ *Nota:* bot responde sua pergunta do brainly
 ║ *[3]*${prefix}Toimg
+║ *Nota:* transforma de figurinha em imagem
 ║ *[4]*${prefix}Ytsearch
+║ *Nota:* procura algo no youtube
 ║ *[5]*${prefix}Darkjokes
-║ *[6]*${prefix}Ytmp4
+║ *Nota:* memes
+║ *[6]*${prefix}Ytmp4 [link]
+║ *Nota:* baixa um video do yt, (em mp4)
 ║ *[7]*${prefix}Tts
+║ *Nota:* transforma de texto em audio
 ║ *[8]*${prefix}Tomp3
-║ *[9]*${prefix}Url2img
+║ *Nota:* transforma de mp4 para mp3
+║ *[9]*${prefix}Url2img 
+║ *Nota:* mostra o url da imagem
 ║ *[10]*${prefix}Dono
+║ *Nota:* causs
 ║ *[11]*${prefix}Dono2
+║ *Nota:* kanzinho
 ║ *[12]*${prefix}Ping
+║ *Nota:* tempo de ativo do gp
 ║ *[13]*${prefix}Gay [@]
+║ *Nota:* porcentagem de gay 
 ║ *[14]*${prefix}Wame ou ${prefix}wa.me
+║ *Nota:* seu link de convite
 ║
 ╚─━━[ KAZINHO BOT ]━━─╝`
 }
