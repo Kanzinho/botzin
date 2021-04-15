f76ac3908655ae6c856ace14a4d125f2e9961272
Kanzinho <80663033+Kanzinho@users.noreply.github.com>
2021-04-15 07:15:23 -0300

    Create cmds.js

---
+++ src/cmds.js
@@ -0,0 +1,18 @@
+const comandos = (prefix) => {
+
+ return `
+╔─━━━━━━{ COMANDOS ]━━━━━━─╗
+║
+║[1]${prefix}Geral
+║[2]${prefix}Interação
+║[3]${prefix}Imagens
+║[4]${prefix}Anime
+║[5]${prefix}Grupo
+║[6]${prefix}Voz
+║[7]${prefix}Premium
+║[8]${prefix}Musica
+║
+╚─━━━━━━[ KAZINHO BOT ]━━━━━━─╝`
+}
+
+exports.comandos = comandos
